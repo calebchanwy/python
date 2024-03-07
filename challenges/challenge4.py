b5b843e386623825e9468f19c86b4ba32a117fd5
Caleb Chan <caleb.chan@kcl.ac.uk>
2024-03-07 12:14:58 +0000

    Update challenge4 to dynamically iterate through thresholds

--- challenges/challenge4.py
+++ challenges/challenge4.py
@@ -20,16 +20,26 @@ import math
 # <returns>Total stamp duty</returns>
 
 
+# Helper method that returns total tax based on a given minimum threshold and rate
+# <param name="THRESHOLD">The minimum threshold that tax applies</param>
+# <param name="value">The current property value</param>
+# <param name="rate">Rate that the tax should be applied at</param>
+# <returns>Tax amount of value, based on a given minimum threshold and rate</returns>
+def TaxAtThreshold(THRESHOLD, value, rate):
+    return (value - THRESHOLD) * rate
+
 def StampDuty(propertyPrice):
     
     # The below can be improved by using enums instead of hard-coding
     # However, I was not too sure if I could import enums for the test or not
     # so I refrained from using them, but used local vars instead
+    # In this case, if any tax rates  or thresholds change, it will be easy to 
+    # update them here
     
     FIRST_THRESHOLD = 300001
     SECOND_THRESHOLD = 925001
     THIRD_THRESHOLD = 1500001
-    
+
     # This dictionairy maps minimum thresholds to tax rates
     rates = {
         FIRST_THRESHOLD : 0.05,
@@ -37,28 +47,27 @@ def StampDuty(propertyPrice):
         THIRD_THRESHOLD : 0.12,
     }
     
-    # Property price below any threshold, (0%)
-    if propertyPrice <= FIRST_THRESHOLD:
+    # Array which contains all our thresholds
+    thresholds = [FIRST_THRESHOLD, SECOND_THRESHOLD, THIRD_THRESHOLD]
+    # Sort array to descending order
+    thresholds.sort(reverse=True)
+    
+    # Property price below any threshold (lower than lowest threshold) (0%)
+    if propertyPrice <= thresholds[-1]:
         return 0
     
     startingValueForTax = propertyPrice
     tax = 0
 
-    # Property price at £1,500,000 threshold (12%)
-    if startingValueForTax >= THIRD_THRESHOLD:
-        tax += (startingValueForTax - THIRD_THRESHOLD)*rates[THIRD_THRESHOLD]
-        # Set value at maximum for next calculation
-        startingValueForTax = THIRD_THRESHOLD -1
+    # Iterating through our thresholds in descending order
 
-    # Property price at £925,000 threshold (10%)
-    if startingValueForTax >= SECOND_THRESHOLD:
-        tax += (startingValueForTax - SECOND_THRESHOLD)*rates[SECOND_THRESHOLD]
-        # Set value at maximum for next calculation
-        startingValueForTax = SECOND_THRESHOLD -1
-        
-    # Property price at £300,000 threshold (5%)
-    if startingValueForTax >= FIRST_THRESHOLD:
-        tax += (startingValueForTax - FIRST_THRESHOLD)*rates[FIRST_THRESHOLD]
-
+    for THRESHOLD in thresholds:
+        # If current value exceeds current threshold
+        if startingValueForTax >= THRESHOLD:
+            # Add tax at threshold
+            tax += TaxAtThreshold(THRESHOLD, startingValueForTax, rates[THRESHOLD])
+            # Set value at maximum for next calculation
+            startingValueForTax = THRESHOLD -1
+            
     # Return final tax rounded nearest integer
     return round(tax, 0)
